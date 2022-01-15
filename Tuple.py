4daa269741bcc50ed980f20bdeab13f66fa655aa
Punita168 <97798860+Punita168@users.noreply.github.com>
2022-01-15 21:56:08 +0530

    Add files via upload

---
+++ Tuple.py
@@ -0,0 +1,51 @@
+#D21CE168 - Punita Chapla
+
+#Practical - 1
+# Write a Python program to create a tuple with different data types.
+
+#Declaration
+a = ("D21CE168",168,True)
+#Print
+print(a)
+
+#--------------------------------------------------------------------------------------------------------
+
+#Practical - 2
+# Write a Python program to create a tuple with numbers and print one item.
+
+#Declaration
+a = (10,220,500,630,42)
+#Print
+print(a[2])
+
+#--------------------------------------------------------------------------------------------------------
+
+#Practical - 3
+# Write a Python program to add an item in a tuple.
+
+#Decalaration
+a = (10,20,30,40,50,60)
+#Print
+print(a+(70,))
+
+#--------------------------------------------------------------------------------------------------------
+
+#Practical - 4
+# Write a Python program to convert a tuple to a string.
+
+#Declaration
+a = ('F','l','o','w','e','r')
+#Join the elements
+b = ''.join(a)
+#Print
+print(b)
+
+#--------------------------------------------------------------------------------------------------------
+
+#Practical - 5
+# Write a Python program to find the length of a tuple.
+
+#Declaration
+a = (1,2,3,4,5,6,7,8,9,10,11,12,15,46)
+#Print
+print(len(a))
